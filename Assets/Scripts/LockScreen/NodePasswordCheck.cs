9029913b4d2b17ada446eece1e3cb44ef51ff1a4
Andrey <andrey@entailegames.ru>
2020-03-16 13:52:27 +0300

    Create passwordCheck

---
+++ Assets/Scripts/LockScreen/NodePasswordCheck.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace LockScreen
+{
+    public class NodePasswordCheck : IPasswordCheck
+    {
+        public void CheckPassword(string password)
+        {
+            Debug.LogWarning("check complite " + (PlayerPrefs.GetString("Password")==password).ToString());
+        }
+    }
+}
