76ebdc800d12a9db7e9f758bb5ab299f6a9cb86f
Andrey <andrey@entailegames.ru>
2020-03-16 13:51:22 +0300

    Create PasswordSet

---
+++ Assets/Scripts/LockScreen/IPasswordSet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace LockScreen
+{
+    public delegate void PasswordIsSet();
+    public interface IPasswordSet
+    {
+        PasswordIsSet passwordIsSet { get; set; }
+
+        void SetPassword(string password);
+    }
+}
---
+++ Assets/Scripts/LockScreen/PasswordSet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace LockScreen
+{
+    public class PasswordSet : IPasswordSet
+    {
+
+        private PasswordIsSet passwordIsSetDel;
+
+        public PasswordIsSet passwordIsSet { get => passwordIsSetDel; set => passwordIsSetDel = value; }
+
+        public void SetPassword(string password)
+        {
+            PlayerPrefs.SetString("Password",password);
+            if(passwordIsSet!=null)
+            {
+                passwordIsSet();
+            }
+        }
+    }
+}
